5d69276e7ce05066e29ffb31570c9eed96dc333b
jonathan Morton <jmorton4193@gmail.com>
2017-09-30 22:49:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 
 function removeElementFromBeginningOfArray (array){
   var myArray = array.slice(1)
-  return array
+  return myArray
 }
