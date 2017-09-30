4414bc44ddf2b85b17ede0e966191da264dfa2c8
jonathan Morton <jmorton4193@gmail.com>
2017-09-30 22:56:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,6 +43,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 function removeElementFromEndOfArray(array){
 vary myArray=array.slice(0,array.length()-1)
-return myArray 
-  
+return myArray
+
 }
