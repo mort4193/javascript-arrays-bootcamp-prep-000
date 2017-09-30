ca15fa8275c3fc227ecdfa8c975d84554694ef5b
jonathan Morton <jmorton4193@gmail.com>
2017-09-30 21:51:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,28 @@
+<<<<<<< HEAD
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
   var myArray = [element, ...array]
+=======
+var chocolateBars=['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+  var myNewArray=[element, ...array]
+  return myNewArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var myNewArray=[...array, element]
+  return myNewArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+>>>>>>> 273c4d909cca5bd7d5ccd3f275c0465665770f69
   return array
 }
