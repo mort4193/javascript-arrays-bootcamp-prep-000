d807cbbb62e24e7291f23c48e18ef2b9f1a2c01f
jonathan Morton <jmorton4193@gmail.com>
2017-09-30 22:48:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray (array){
-  array.splice(2,3)
+  var myArray = array.slice(1)
   return array
 }
